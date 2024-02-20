e3a2af4f834f4c201b1560d60a3d137ef636a661
tonnymuchui <tonykanyingah@gmail.com>
2024-02-20 10:45:45 +0300

    Add User data

---
+++ app/src/main/java/com/ecommerce/data/User.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.data
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    var imagePath: String = ""
+){
+    constructor(): this("","","","")
+
+}
