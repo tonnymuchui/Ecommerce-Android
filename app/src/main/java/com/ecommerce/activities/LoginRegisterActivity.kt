ee1a0e51a7eb25deadb750d23c508185f31f1e0b
tonnymuchui <tonykanyingah@gmail.com>
2024-02-28 02:30:26 +0300

    Annotate AndroidEntryPoint

--- app/src/main/java/com/ecommerce/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/ecommerce/activities/LoginRegisterActivity.kt
@@ -3,8 +3,15 @@ package com.ecommerce.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ecommerce.R
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
