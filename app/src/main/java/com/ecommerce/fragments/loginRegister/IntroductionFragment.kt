fab0dd0161dd410ef921eb0bb839d8bab3ad47b8
tonnymuchui <tonykanyingah@gmail.com>
2024-02-28 02:31:32 +0300

    Add IntroductionFragment

--- app/src/main/java/com/ecommerce/fragments/loginRegister/IntroductionFragment.kt
+++ app/src/main/java/com/ecommerce/fragments/loginRegister/IntroductionFragment.kt
@@ -1,7 +1,33 @@
 package com.ecommerce.fragments.loginRegister
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ecommerce.R
+import com.ecommerce.databinding.FragmentIntroductionBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
+    private lateinit var binding: FragmentIntroductionBinding;
+    private val navController by lazy { findNavController() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentIntroductionBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonStart.setOnClickListener {
+            navController.navigate(R.id.action_introductionFragment_to_accountOptionsFragment)
+        }
+    }
 }
