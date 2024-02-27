351b867813e627793d2b927c533b6739592b30dc
tonnymuchui <tonykanyingah@gmail.com>
2024-02-28 02:34:08 +0300

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-    id ("kotlin-parcelize")
+    id("kotlin-parcelize")
     id("androidx.navigation.safeargs.kotlin")
-    kotlin("kapt")
+    id ("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
 
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -44,7 +44,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -53,41 +52,51 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
-    //Navigation component
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+    // Hilt
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("com.google.dagger:hilt-android:2.49")
+    kapt("com.google.dagger:hilt-compiler:2.49")
+    implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
+    // Navigation component
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
-    //loading button
-    implementation ("br.com.simplepass:loading-button-android:2.2.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 
-    //Glide
-    implementation ("com.github.bumptech.glide:glide:4.13.0")
+    // loading button
+    implementation("br.com.simplepass:loading-button-android:2.2.0")
 
-    //circular image
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
 
-    //viewpager2 indicatior
-    implementation ("io.github.vejei.viewpagerindicator:viewpagerindicator:1.0.0-alpha.1")
+    // circular image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
-    //stepView
-    implementation ("com.shuhart.stepview:stepview:1.5.1")
+    // viewpager2 indicator
+    implementation("io.github.vejei.viewpagerindicator:viewpagerindicator:1.0.0-alpha.1")
 
-    //Android Ktx
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    // stepView
+    implementation("com.shuhart.stepview:stepview:1.5.1")
 
-    //Coroutines with firebase
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.1")
-    //Android Ktx
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    //Dimen
-    implementation ("com.intuit.ssp:ssp-android:1.0.5")
-    implementation ("com.intuit.sdp:sdp-android:1.0.5")
+    // Android Ktx
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
 
-//    Firebase Auth
+    // Coroutines with firebase
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+
+    // Dimen
+    implementation("com.intuit.ssp:ssp-android:1.0.5")
+    implementation("com.intuit.sdp:sdp-android:1.0.5")
+
+    // Firebase Auth
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-firestore:24.10.2")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-auth:22.3.1")
+}
+kapt {
+    correctErrorTypes = true
 }
