5390b861e33c6f0c9d03e42abce884b641d1afff
tonnymuchui <tonykanyingah@gmail.com>
2024-02-28 02:26:48 +0300

    Add Resource

--- app/src/main/java/com/ecommerce/util/Resource.kt
+++ app/src/main/java/com/ecommerce/util/Resource.kt
@@ -1,10 +1,11 @@
 package com.ecommerce.util
 
-sealed class Resource<T> (
+sealed class Resource<T>(
     val data: T? = null,
     val message: String? = null
-){
+) {
     class Success<T>(data: T): Resource<T>(data)
     class Error<T>(message: String): Resource<T>(message = message)
     class Loading<T>: Resource<T>()
+    class Unspecified<T> : Resource<T>()
 }
