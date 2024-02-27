0ef832ecf3ad06e9ff6d764889cc186c8aae6035
tonnymuchui <tonykanyingah@gmail.com>
2024-02-28 02:35:30 +0300

    Add AccountOptions

--- app/src/main/java/com/ecommerce/fragments/loginRegister/AccountOptions.kt
+++ app/src/main/java/com/ecommerce/fragments/loginRegister/AccountOptions.kt
@@ -1,7 +1,31 @@
 package com.ecommerce.fragments.loginRegister
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ecommerce.R
+import com.ecommerce.databinding.FragmentAccountOptionsBinding
 
 class AccountOptions: Fragment(R.layout.fragment_account_options) {
+    private lateinit var binding: FragmentAccountOptionsBinding;
+    private val navController by lazy { findNavController() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAccountOptionsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonRegisterAccountOptions.setOnClickListener {
+            navController.navigate(R.id.action_accountOptionsFragment_to_registerFragment)
+        }
+    }
 }
