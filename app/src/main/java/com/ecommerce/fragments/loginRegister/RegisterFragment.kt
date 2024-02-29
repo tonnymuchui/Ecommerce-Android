c146ddebf4e4ad71529591cd396657fca6f3c82c
tonnymuchui <tonykanyingah@gmail.com>
2024-02-29 15:17:21 +0300

    Add navigate loginFragment

--- app/src/main/java/com/ecommerce/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/ecommerce/fragments/loginRegister/RegisterFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.ecommerce.R
 import com.ecommerce.data.User
 import com.ecommerce.databinding.FragmentRegisterBinding
@@ -38,6 +39,9 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tvDoYouHaveAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
         binding.apply {
             buttonRegisterRegister.setOnClickListener {
                 val user = User(
