e62d964f25d4a100ca7f5fd48a24e1855c05c099
tonnymuchui <tonykanyingah@gmail.com>
2024-02-29 15:18:07 +0300

    Add LoginFragment

--- app/src/main/java/com/ecommerce/fragments/loginRegister/LoginFragment.kt
+++ app/src/main/java/com/ecommerce/fragments/loginRegister/LoginFragment.kt
@@ -1,7 +1,101 @@
 package com.ecommerce.fragments.loginRegister
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.ecommerce.R
+import com.ecommerce.activities.ShoppingActivity
+import com.ecommerce.databinding.FragmentLoginBinding
+import com.ecommerce.dialog.setupBottomSheetDialog
+import com.ecommerce.util.Resource.*
+import com.ecommerce.viewModel.LoginViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class LoginFragment: Fragment(R.layout.fragment_login) {
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvDontHaveAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.tvDontHaveAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.apply {
+            buttonLoginLogin.setOnClickListener {
+                val email = edEmailLogin.text.toString().trim()
+                val password = edPasswordLogin.text.toString()
+                viewModel.login(email, password)
+            }
+        }
+
+        binding.tvForgotPasswordLogin.setOnClickListener {
+            setupBottomSheetDialog { email ->
+                viewModel.resetPassword(email)
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.resetPassword.collect{
+                when (it) {
+                    is Loading -> {
+                    }
+                    is Success -> {
+                        Snackbar.make(requireView(),"Reset link was sent to your email",Snackbar.LENGTH_LONG).show()
+                    }
+                    is Error -> {
+                        Snackbar.make(requireView(),"Error: ${it.message}",Snackbar.LENGTH_LONG).show()
+                    }
+                    else -> Unit
+
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.login.collect {
+                when (it) {
+                    is Loading -> {
+                        binding.buttonLoginLogin.startAnimation()
+                    }
+                    is Success -> {
+                        binding.buttonLoginLogin.revertAnimation()
+                        Intent(requireActivity(), ShoppingActivity::class.java).also { intent ->
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                        }
+                    }
+                    is Error -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                        binding.buttonLoginLogin.revertAnimation()
+                    }
+                    else -> Unit
+
+                }
+            }
+        }
+    }
 }
