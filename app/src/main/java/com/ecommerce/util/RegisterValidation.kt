fa72b0a6e24270be52f12a7beaa61be75d668b38
tonnymuchui <tonykanyingah@gmail.com>
2024-02-29 13:00:14 +0300

    Add RegisterValidation

---
+++ app/src/main/java/com/ecommerce/util/RegisterValidation.kt
@@ -0,0 +1,10 @@
+package com.ecommerce.util
+
+sealed class RegisterValidation() {
+    data object Success: RegisterValidation()
+    data class Failed(val message: String): RegisterValidation();
+}
+data class RegisterFieldsState(
+    val email: RegisterValidation,
+    val password: RegisterValidation
+)
